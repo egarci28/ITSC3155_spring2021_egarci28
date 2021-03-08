e56119a9699f9c7adc713db459e65db541c833c5
EG28 <egarci28@uncc.edu>
2021-03-08 13:17:49 -0500

    PythonBasics2 - Update methods

--- PythonBasics2/pythonBasics2.py
+++ PythonBasics2/pythonBasics2.py
@@ -12,40 +12,77 @@
 
 def count_threes(n):
   # YOUR CODE HERE
-  x = 0; # Hold the number of threes.
-  x1 = 0; # Use to count from 0 to n.
-  while x1 < n:
-      x1 = 3 + x1;
-      if x1 <= n:
-          x += 1;
-  return x; 
-  
+  # Dictionary.
+  nums = {
+      "3": 0,
+      "6": 0,
+      "9": 0,
+  }
 
+  # Variables + Constants.
+  highest = 0;
+  value = 0;
+  
+  # Find all multiples of 3 and count them.
+  for x in n:
+      if x == '3':
+          nums["3"] += 1;
+      elif x == '6':
+          nums["6"] += 1;
+      else:
+          nums["9"] += 1;
+          
+  # Find the multiple of 3 with the highest count.
+  for x, x1 in nums.items():
+      if x1 > highest:
+          highest = x1;
+          value = x;
+  # Return the multiple of 3 with the highest count.
+  return int(value); 
 
 # Part B. longest_consecutive_repeating_char
 # Define a function longest_consecutive_repeating_char(s) that takes
 # a string s and returns the character that has the longest consecutive repeat.
 def longest_consecutive_repeating_char(s):
   # YOUR CODE HERE
-  currentCharCount = 0;
-  currentChar = s[0];
-  longestCharCount = 0;
-  longestChar = s[0];
-  count = 0;
+  # Dictionary.
+  chars = {
+  }
   
-  for i in s:   
+  # Variables + Constants.
+  highest = 0;
+  values = [];
+  currentChar ='0';
+  longestCharCount = 0;
+  currentCharCount = 0;
+  
+  # Determine the longest consecutive repeating chars.
+  for i in s:
+     # Add a char if it is not in the dictionary.
      if currentChar != i:
          currentChar = i;
-         currentCharCount = 0;
-         currentChar = i;
-     if longestCharCount < currentCharCount:
+         chars[i] = 0;
+     # Save the current char if it is longer than the previous one.
+     if longestCharCount <= currentCharCount:
          longestCharCount = currentCharCount;
-         longestChar = i; 
-     currentCharCount += 1;
-     count += 1;
-      
-  return longestChar;
+         # Set all other chars to 0 when a new longest consecutive char is found.
+         for x in values:
+             if chars[x] < chars[i]:
+                 chars[x] = 0; 
+     # Add 1 to the current char value.
+     chars[i] += 1;
   
+  # Find the highest value.
+  for x, x1 in chars.items():
+      if x1 > highest:
+          highest = x1;
+  # Find the chars with the highest values.
+  
+  for x, x1 in chars.items():
+      if x1 == highest:
+          values.append(x);
+  # Return the chars with the highest values.
+  return values;
 
 # Part C. is_palindrome
 # Define a function is_palindrome(s) that takes a string s
@@ -54,8 +91,7 @@ def longest_consecutive_repeating_char(s):
 # forwards. Treat capital letters the same as lowercase ones
 # and ignore spaces (i.e. case insensitive).
 def is_palindrome(s):
-  # YOUR CODE HERE
-  
+  # YOUR CODE HERE  
   s.lower(); # Convert string to lowercase.
   x = True;
   count = len(s)-1;
@@ -65,8 +101,4 @@ def is_palindrome(s):
         break;
     count -= 1;
   return x;
-  
-    
-  '''
-  EG28
-  '''
+ 
