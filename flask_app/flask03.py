31aaec330d972acfa69c8391e9c0d44122ec8167
EG28 <egarci28@uncc.edu>
2021-03-29 14:45:24 -0400

    flask app #3 - added execution path for POST request for new note

--- flask_app/flask03.py
+++ flask_app/flask03.py
@@ -4,6 +4,7 @@
 import os                 # os is used to get environment variables IP & PORT
 from flask import Flask   # Flask is the web app that we will customize
 from flask import render_template
+from flask import request
 
 
 app = Flask(__name__)     # create an app
@@ -31,10 +32,15 @@ def getnote(note_id):
              3: {'title': 'Third Note', 'text': 'This is my third note', 'date': '10-3-2020'}
             }
     return render_template('note.html', note = notes[int(note_id)], user = a_user)
-@app.route('/notes/new') 
+@app.route('/notes/new', methods = ['GET', 'POST']) 
 def newnote(): 
     a_user = {'name': 'Eliseo', 'email':'email'}
-    return render_template('new.html', user=a_user)
+    # Check method used for request. 
+    print ('request method is', request.method) 
+    if request.method == 'POST': 
+        return '<h1> POST method used for this request </h1>' 
+    else: 
+        return render_template('new.html', user = a_user)
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
 
